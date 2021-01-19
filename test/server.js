497a067eb90600824e61cb2e13fd6cb5603dcf6c
duy <ddao0892@gmail.com>
2021-01-19 15:28:22 -0700

    phase 2

--- test/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const port = 3000
-
-const server = http.createServer(function(require, response) {
-    response.end()
-}).listen(port, function() {
-    console.log(`This ${port} port works!!! `)
-})
